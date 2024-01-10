0fbb2a4ac82ddd2e24ee1484e59d74f656d73a94
mohdsahil36 <mohdsahilsiddiqui36@gmail.com>
2024-01-10 13:11:58 +0530

    sidebar-profile

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,33 @@
+"use client"
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+
 export default function DocumentsPage(){
+    const {user}=useUser();
     return(
-        <>
-            This is a protected page for documents!
-        </>
+        <div className="h-full flex flex-col items-center justify-center space y-4">
+            <Image
+            src="/empty.png"
+            height="300"
+            width="300"
+            alt="empty"
+            className="dark:hidden"
+            />
+            <Image
+            src="/empty-dark.png"
+            height="300"
+            width="300"
+            alt="empty"
+            className="hidden dark:block"
+            />
+
+            <h2 className="text-large font-medium">Welcome to {user?.fullName}&apos;s Notion</h2>
+            <Button className="mt-3">
+                <PlusCircle className="h-4 w-4 mr-2"/>
+                Create a note
+            </Button>
+        </div>
     )
 }
