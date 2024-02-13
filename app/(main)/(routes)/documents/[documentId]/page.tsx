14f468edf3fd0d0f4a1729d75276c47a99f314cb
mohdsahil36 <mohdsahilsiddiqui36@gmail.com>
2024-02-13 13:11:06 +0530

    toolbar,title of page,icon-select

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,7 +1,45 @@
-export default function DocumentIdPage(){
+"use client"
+
+import { api } from "@/convex/_generated/api";
+import { useQuery } from "convex/react";
+import { Id } from "@/convex/_generated/dataModel";
+import { Toolbar } from "@/app/(main)/_components/Toolbar";
+
+interface DocumentIdProps{
+    params:{
+        documentId:Id<"documents">
+    }
+}
+
+export default function DocumentIdPage({
+    params
+}:DocumentIdProps){
+    const document=useQuery(api.documents.getById,{
+        documentId:params.documentId
+    });
+    
+    if(document===undefined){
+        return(
+            <div>
+                Loading...
+            </div>
+        )
+    }
+
+    if(document===null){
+        return(
+            <div>
+                Not found
+            </div>
+        )
+    }
+
     return(
-        <>
-            This is document [id] page
-        </>
+        <div className="pb-40">
+            <div className="h-[35vh] "></div>
+            <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+                <Toolbar initialData={document}/>
+            </div>
+        </div>
     )
 }
--- convex/documents.ts
+++ convex/documents.ts
@@ -1,6 +1,7 @@
 import {v} from "convex/values";
 import { mutation,query } from "./_generated/server";
 import { Doc,Id } from "./_generated/dataModel";
+import { useMutation } from "convex/react";
 
 
 export const getSidebar = query({
@@ -304,3 +305,33 @@ export const update = mutation({
     return document;
   },
 });
+
+export const removeIcon=mutation({
+  args:{id:v.id("documents")},
+  handler:async(ctx,args)=>{
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Unauthenticated");
+    }
+
+    const userId = identity.subject;
+
+    const existingDocument = await ctx.db.get(args.id);
+
+    if (!existingDocument) {
+      throw new Error("Not found");
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const document = await ctx.db.patch(args.id, {
+      icon:undefined,
+    });
+
+    return document;
+ 
+  }
+})
